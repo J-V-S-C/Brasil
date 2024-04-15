69520a28e898a8118deb9e04cd86a4937e15f062
João Victor Sant'Ana Cortabitart <138387808+J-V-S-C@users.noreply.github.com>
2024-04-15 09:44:39 -0300

    Add files via upload

---
+++ site2/controller/clientesController.php
@@ -0,0 +1,19 @@
+<?php
+if($_POST){
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $datanascimento = $_POST['dt'];
+    $senha = $_POST['senha'];
+    $senha2 = $_POST['senha2'];
+
+
+    if ($senha == $senha2) {
+        //Cadastro o usuário
+        echo 'As senhas conferem.';
+    } else {
+        header('location:../cadastroclientes.php?cod=171');
+    }
+} else {
+    header('location:../cadastroclientes.php?opa');
+}
